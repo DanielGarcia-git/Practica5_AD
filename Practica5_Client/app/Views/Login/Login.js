caa96db3d31b37b1594268eb5d2658fc09b8e455
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2022-12-11 18:09:37 +0100

    Add inicialize function

--- Practica5_Client/app/Views/Login/Login.js
+++ Practica5_Client/app/Views/Login/Login.js
@@ -10,13 +10,24 @@ module.config(['$routeProvider', function($routeProvider) {
 }])
 
 module.controller('LoginCtrl', function($scope) {
+
+  inicializeScopeVar($scope);
+  inicializeScopeFunctions($scope);
+
+  
+});
+
+function inicializeScopeVar($scope) {
   $scope.state = {};
   $scope.state.login = true;
   $scope.state.sigin = false;
   $scope.state.actualPage = "login";
+}
+
+function inicializeScopeFunctions($scope) {
 
   $scope.ShowLogIn = function() {
     $scope.state.login = !$scope.state.login;
     $scope.state.sigin = !$scope.state.login;
   };
-});
+}
