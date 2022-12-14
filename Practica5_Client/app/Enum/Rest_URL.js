8627a46ced96b83c0d117cf63888db801c42e06c
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2022-12-14 17:35:51 +0100

    Registrar Imagen done

--- Practica5_Client/app/Enum/Rest_URL.js
+++ Practica5_Client/app/Enum/Rest_URL.js
@@ -1,6 +1,6 @@
 const REST_URL = {
     BASE: 'http://localhost:8080/Practica4_ServiceREST/api/',
-    LOGIN: 'http://localhost:8080/Practica4_ServiceREST/api/users/login/:username/:password/',
+    LOGIN: 'http://localhost:8080/Practica4_ServiceREST/api/users/login/',
     REGISTER: 'http://localhost:8080/Practica4_ServiceREST/api/users/register/',
     REGISTER_IMAGE: "http://localhost:8080/Practica4_ServiceREST/api/images/register/"
 }
--- Practica5_Client/app/Views/RegistrarImagen/RegistrarImagen.js
+++ Practica5_Client/app/Views/RegistrarImagen/RegistrarImagen.js
@@ -15,24 +15,35 @@ module.factory('Image', ['$resource', function($resource) {
       method: 'POST',
       url: REST_URL.REGISTER_IMAGE,
       headers: {'Content-Type': undefined},
-      transformRequest: function (data, headersGetter) {
-        var str = [];
-        for (var d in data)
-            str.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-        return str.join("&");
+      transformRequest: function (data) {
+        var formData = new FormData();
+        for (var key in data) {
+          if (data[key] instanceof Date) formData.append(key, data[key].toLocaleDateString());
+          formData.append(key, data[key]);
+        }
+        return formData;
       }
     }
   });
 }]);
 
-module.controller('RegistrarImagenCtrl', ['$scope', '$window', '$cookies', function($scope, $window, $cookies) {
+module.controller('RegistrarImagenCtrl', ['$scope', '$window', '$cookies', 'Image', function($scope, $window, $cookies, Image) {
   
-
   var user = $cookies.getObject('user');
-  if (user != null) {
-    $scope.user = user;
-  }
-  else {
-    $window.location.href = "#!/login";
-  }
+  if (user != null) $scope.user = user;
+  else $window.location.href = "#!/login";
+
+  $scope.register_image = function() {
+    $scope.image.file = document.getElementById('uploadImage').files[0];
+    $scope.image.filename = $scope.image.file.name;
+    $scope.image.creator = $scope.user.UserEmail;
+    Image.register($scope.image).$promise.then(function(response) {
+      if (response.success) $window.location.href = "#!/home";
+      else {
+        
+      }
+    }, function(error) {
+      console.log(error);
+    });
+  };
 }]);
